256a017423c6982024ae8f56581cc82d93eb16f6
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-07-18 14:41:43 +0300

    Import Complete

--- CentralRegisterOfNonprofitOrganizations/startUp/Import.cs
+++ CentralRegisterOfNonprofitOrganizations/startUp/Import.cs
@@ -9,6 +9,7 @@ using HtmlAgilityPack;
 using startUp;
 using Microsoft.Office.Interop.Excel;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -19,42 +20,8 @@ namespace ImportHtml
         public static void Main(string[] args)
         {
             var dir = @"C:\Users\Mihail\Documents\visual studio 2017\Projects\OrganizationsDatabase\CentralRegisterOfNonprofitOrganizations\startUp\Models\List.xls";
-
-            var context = new OrganizationsContext();
-            using (context)
-            {
-                var link = context.Organizations.Select(o => o.Link).ToList().First();
-                string obj = string.Empty;
-                string ways = string.Empty;
-                string subjOfActivity = string.Empty;
-                WebClient wc = new WebClient();
-                byte[] webData = wc.DownloadData(link);
-                var text = Encoding.UTF8.GetString(webData);
-
-                var pattern = @"<h4>Предмет на дейност<\/h4>\n\s*<.*>\n\s*<.*>\n\s*<.*>([^%]+)<\/pre>";
-                MatchCollection collection = Regex.Matches(text, pattern);
-
-                foreach (Match match in collection)
-                {
-                    Console.WriteLine(match.Groups[1].Value);
-                }
-
-
-            }
-
         }
-        public static void AddOrganization(Organization org)
-        {
-            var context = new OrganizationsContext();
 
-            using (context)
-            {
-                context.Organizations.Add(org);
-                context.SaveChanges();
-            }
-            Console.WriteLine($"Organization {org.Name} added to the database!");
-
-        }
         public static void ReadFromExcel(string directory)
         {
 
@@ -65,7 +32,7 @@ namespace ImportHtml
             Console.WriteLine("Press any key to start importing organizations.");
             Console.ReadKey();
 
-            for (int i = 2; i < 50; i++)
+            for (int i = 2; i < 1048; i++)
             {
                 var orgName = xlRange.Cells[i, 1].Value2.ToString();
                 var type = xlRange.Cells[i, 2].Value2.ToString();
@@ -102,8 +69,13 @@ namespace ImportHtml
                 string objectives = string.Empty;
                 string ways = string.Empty;
                 string subjectOfActivity = string.Empty;
-                //Problem
-                ExtractInfoFromHyperlink(hyperlinkAdress, objectives, ways, subjectOfActivity);
+                List<string> tokens = ExtractInfoFromHyperlink(hyperlinkAdress);
+
+
+                objectives = tokens[0];
+                ways = tokens[1];
+                subjectOfActivity = tokens[2];
+
 
                 Organization org = new Organization()
                 {
@@ -118,25 +90,38 @@ namespace ImportHtml
                     Link = hyperlinkAdress
                 };
 
+                //Console.WriteLine(org.Name.ToUpper());
+                //Console.WriteLine("ЦЕЛИ:" + Environment.NewLine + org.Objectives);
+                //Console.WriteLine("СРЕДСТВА:" + Environment.NewLine + org.Ways);
+                //Console.WriteLine("ПРЕДМЕТ НА ДЕЙНОСТ:" + Environment.NewLine + org.SubjectOfActivity);
+                //Console.WriteLine();
+
+
                 AddOrganization(org);
             }
+            Console.WriteLine("SUCCESS !!!");
         }
-        public static void ExtractInfoFromHyperlink(string adress, string obj, string ways, string subjOfActivity)
+        public static List<string> ExtractInfoFromHyperlink(string adress)
         {
+            string obj = String.Empty;
+            string ways = string.Empty;
+            string subjOfActivity = string.Empty;
+            List<string> toReturn = new List<string>();
             WebClient wc = new WebClient();
             byte[] webData = wc.DownloadData(adress);
             var text = Encoding.UTF8.GetString(webData);
-            var objectivesPattern = @"<h4>Цели<\/h4>\n\s*<.*>\n\s*<.*>\n\s*<.*>([А-Яа-я\s:;.,\w\\'""-=]*)<\/pre>";
-            var waysPattern = @"<h4>Средства<\/h4>\n\s*<.*>\n\s*<.*>\n\s*<.*>([А-Яа-я\s:;.,\w\\""-=]*)<\/pre>";
-            var subjectOfActivityPattern = @"<h4>Предмет на дейност<\/h4>\n\s*<.*>\n\s*<.*>\n\s*<.*>([А-Яа-я\s:;.,\w\\'""]*)<\/pre>";
-
+            var objectivesPattern = @"<h4>Цели<\/h4>\n*\s*<.*>\n*\s*<.*>\n*\s*<.*>([^%]+)<\/pre>";
+            var waysPattern = @"<h4>Средства<\/h4>\n*\s*<.*>\n*\s*<.*>\n*\s*<.*>([^%]+)<\/pre>";
+            var subjectOfActivityPattern = @"<h4>Предмет на дейност<\/h4>\n*\s*<.*>\n*\s*<.*>\n*\s*<.*>([^%]+)<\/pre>";
+            
             MatchCollection objectivesCollection = Regex.Matches(text, objectivesPattern);
             MatchCollection waysCollection = Regex.Matches(text, waysPattern);
             MatchCollection subjectOfActivityCollection = Regex.Matches(text, subjectOfActivityPattern);
 
             if (objectivesCollection.Count > 0)
             {
-                obj = objectivesCollection[0].Groups[1].Value.ToString();
+                List<string> objFull = objectivesCollection[0].Groups[1].Value.Split(new string[] { "</div>" }, StringSplitOptions.None).ToList();
+                obj = objFull[0].Trim();
             }
             else
             {
@@ -144,7 +129,8 @@ namespace ImportHtml
             }
             if (waysCollection.Count > 0)
             {
-                ways = waysCollection[0].Groups[1].Value.ToString();
+                var waysFull = waysCollection[0].Groups[1].Value.Split(new string[] { "</div>" }, StringSplitOptions.None).ToList();
+                ways = waysFull[0].Trim();
             }
             else
             {
@@ -152,17 +138,39 @@ namespace ImportHtml
             }
             if (subjectOfActivityCollection.Count > 0)
             {
-                subjOfActivity = subjectOfActivityCollection[0].Groups[1].Value.ToString();
+                var subjOfActivityFull = subjectOfActivityCollection[0].Groups[1].Value.Split(new string[] { "su</div>" }, StringSplitOptions.None).ToList();
+                subjOfActivity = subjOfActivityFull[0].Trim();
             }
             else
             {
                 subjOfActivity = "";
             }
+            toReturn.Add(obj);
+            toReturn.Add(ways);
+            toReturn.Add(subjOfActivity);
+            return toReturn;
         }
+        public static void AddOrganization(Organization org)
+        {
+            var context = new OrganizationsContext();
 
+            using (context)
+            {
+                if (context.Organizations.Where(o => o.Name == org.Name).FirstOrDefault() == null)
+                {
+                    context.Organizations.Add(org);
+                    context.SaveChanges();
+                    Console.WriteLine($"{org.Name.ToUpper()} added to the database successfully!"); 
+                }
+                else
+                {
+                    Console.WriteLine($"Organization {org.Name} is already in the Organization database! Org. NOT added.");
+                }
+            }
+
+
+        }
     }
-//var pattern = @"<h4>Предмет на дейност<\/h4>\n*\s*<.*>\n*\s*<.*>\n*\s*<.*>([^%]+)<\/pre>";
-
 }
 
 
